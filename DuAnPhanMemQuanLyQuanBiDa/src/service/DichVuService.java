c64017cde5655444ad357f8d6f029439f99071d5
Quocanh1306 <109933873+Quocanh1306@users.noreply.github.com>
2023-04-07 22:55:01 +0700

    qa

--- DuAnPhanMemQuanLyQuanBiDa/src/service/DichVuService.java
+++ DuAnPhanMemQuanLyQuanBiDa/src/service/DichVuService.java
@@ -4,6 +4,7 @@
  */
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 import model.DichVu;
 import repository.DichVuRepository;
@@ -25,5 +26,24 @@ public class DichVuService {
     }
     public int delete(String ma) {
         return repo.deleteDichVuRepository(ma);
+    }
+    public List<DichVu> dichVuNuoc() {
+        List<DichVu> list = new ArrayList<>();
+        for (DichVu dv : repo.getAll()) {
+            if(dv.getLoaiDV().equalsIgnoreCase("nuoc")) {
+                list.add(dv);
+            }
+        }
+        return list;
+    }
+    
+    public List<DichVu> dichGay() {
+        List<DichVu> list = new ArrayList<>();
+        for (DichVu dv : repo.getAll()) {
+            if(dv.getLoaiDV().equalsIgnoreCase("gay")) {
+                list.add(dv);
+            }
+        }
+        return list;
     }
 }
